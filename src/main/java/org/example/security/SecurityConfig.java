d78646d20aee2c0d846ebeb3f7abf13a0c1bdaee
ShreyaSingh2910 <shreyapcm20@gmail.com>
2026-02-12 00:05:37 +0530

    Update SecurityConfig.java

--- src/main/java/org/example/security/SecurityConfig.java
+++ src/main/java/org/example/security/SecurityConfig.java
@@ -5,9 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 @Configuration
 public class SecurityConfig {
+    private final JwtFilter jwtFilter;
 
+    public SecurityConfig(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
@@ -36,10 +42,10 @@ public class SecurityConfig {
 
                         // everything else
                         .anyRequest().permitAll()
-                );
+
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
 }
-
-
